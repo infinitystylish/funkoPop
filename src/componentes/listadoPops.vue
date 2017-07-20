faf8e94731f7890d2ce971ac03fe4f93232e46ce
infinitystylish <draco_ghost@live.com.mx>
2017-07-20 06:49:23 -0500

    Agregado filtro de busqueda

--- src/componentes/listadoPops.vue
+++ src/componentes/listadoPops.vue
@@ -5,103 +5,152 @@
 				<h1>Listado de Funko Pop</h1>
 			</div>
 			<div class="panel-body">
-				<div class="table-responsive">
-					<table class="table table-hover table-bordered table-funkopop">
-						<thead>
-							<tr>
-								<th>
-									ID
-								</th>
-								<th>
-									Licencia
-								</th>
-								<th>
-									Nombre
-								</th>
-								<th>
-									Número de pop
-								</th>
-								<th>
-									Cantidad comprada
-								</th>
-								<th>
-									Cantidad disponible
-								</th>
-								<th>
-									Vendidos
-								</th>
-								<th>
-									Costo
-								</th>
-								<th>
-									Costo total
-								</th>
-								<th>
-									Precio a público
-								</th>
-								<th>
-									Margen de ganancia
-								</th>
-								<th>
-									Descuentos
-								</th>
-								<th>
-									Ganancia
-								</th>
-								<th>
-									Recuperación de dinero
-								</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr v-for="(pop, indice) of pops" v-bind:class="{ 'table-danger' : disponibilidadFunko(pop)}">
-
-								<td>
-									{{indice + 1}} 
-								</td>
-								<td>
-									{{ pop.licencia }}
-								</td>
-								<td>
-									{{ pop.nombre }}
-								</td>
-								<td>
-									{{ pop.numeroPop }}
-								</td>
-								<td>
-									{{ pop.cantidadComprada }}
-								</td>
-								<td>
-									{{ pop.cantidadDisponible }}
-								</td>
-								<td>
-									{{ pop.vendidos }}
-								</td>
-								<td>
-									{{ pop.costo }}
-								</td>
-								<td>
-									{{ costoTotal(pop) }}
-								</td>
-								<td>
-									{{ pop.precioPublico }}
-								</td>
-								<td>
-									{{ margenGanancia(pop) }}
-								</td>
-								<td>
-									{{ pop.descuentos }}
-								</td>
-								<td>
-									{{ ganancia(pop) }}
-								</td>
-								<td>
-									{{ vendidos(pop) }}
-								</td>
-							</tr>
-						</tbody>
-					</table>
+				<div class="form-group">
+				    <input type="text" class="form-control" placeholder="Buscar Pop" v-model="buscar">
 				</div>
+				<table class="table table-hover table-bordered table-funkopop">
+					<thead>
+						<tr>
+							<th>
+								ID
+							</th>
+							<th>
+								Licencia
+							</th>
+							<th>
+								Nombre
+							</th>
+							<th>
+								Número de pop
+							</th>
+							<th>
+								Cantidad comprada
+							</th>
+							<th>
+								Cantidad disponible
+							</th>
+							<th>
+								Vendidos
+							</th>
+							<th>
+								Costo
+							</th>
+							<th>
+								Costo total
+							</th>
+							<th>
+								Precio a público
+							</th>
+							<th>
+								Margen de ganancia
+							</th>
+							<th>
+								Descuentos
+							</th>
+							<th>
+								Ganancia
+							</th>
+							<th>
+								Recuperación de dinero
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr v-show="buscar" v-for="(pop, indice) in buscarPop" class="table-info">
+							<td v-if="disponibilidadFunko(pop)">
+								 No hay disponibles  
+							</td>
+							<td v-else>
+								Hay {{pop.cantidadDisponible}} disponibles
+							</td>
+							<td>
+								{{ pop.licencia }}
+							</td>
+							<td>
+								{{ pop.nombre }}
+							</td>
+							<td>
+								{{ pop.numeroPop }}
+							</td>
+							<td>
+								{{ pop.cantidadComprada }}
+							</td>
+							<td>
+								{{ pop.cantidadDisponible }}
+							</td>
+							<td>
+								{{ pop.vendidos }}
+							</td>
+							<td>
+								{{ pop.costo }}
+							</td>
+							<td>
+								{{ costoTotal(pop) }}
+							</td>
+							<td>
+								{{ pop.precioPublico }}
+							</td>
+							<td>
+								{{ margenGanancia(pop) }}
+							</td>
+							<td>
+								{{ pop.descuentos }}
+							</td>
+							<td>
+								{{ ganancia(pop) }}
+							</td>
+							<td>
+								{{ vendidos(pop) }}
+							</td>
+						</tr>
+
+						<tr v-for="(pop, indice) of pops" v-bind:class="{ 'table-danger' : disponibilidadFunko(pop)}">
+
+							<td>
+								{{indice + 1}} 
+							</td>
+							<td>
+								{{ pop.licencia }}
+							</td>
+							<td>
+								{{ pop.nombre }}
+							</td>
+							<td>
+								{{ pop.numeroPop }}
+							</td>
+							<td>
+								{{ pop.cantidadComprada }}
+							</td>
+							<td>
+								{{ pop.cantidadDisponible }}
+							</td>
+							<td>
+								{{ pop.vendidos }}
+							</td>
+							<td>
+								{{ pop.costo }}
+							</td>
+							<td>
+								{{ costoTotal(pop) }}
+							</td>
+							<td>
+								{{ pop.precioPublico }}
+							</td>
+							<td>
+								{{ margenGanancia(pop) }}
+							</td>
+							<td>
+								{{ pop.descuentos }}
+							</td>
+							<td>
+								{{ ganancia(pop) }}
+							</td>
+							<td>
+								{{ vendidos(pop) }}
+							</td>
+						</tr>
+					</tbody>
+				</table>
 			</div>
 		</div>
 		<div class="button-list-container pull-right">
@@ -115,7 +164,7 @@
 		props: ['pops'],
 		data() {
 		    return {
-		      
+		    	buscar: ''
 		    }
 		},
 		methods:{
@@ -142,7 +191,12 @@
 			vendidos(pop){
 				return pop.vendidos * pop.costo;
 			}
-		}
+		},
+		computed:{
+			buscarPop(){
+				return this.pops.filter((pop) => pop.nombre.toLowerCase().includes(this.buscar));
+			}
+		} 
 	}
 </script>
 
@@ -167,6 +221,10 @@
 
 	.table-danger{	    	
 		background-color: #f2dede;
+	}
+
+	.table-info{	    	
+		background-color: #d9edf7;
 	}
 
 </style>
